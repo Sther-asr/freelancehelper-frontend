6131097678c538d4da9eafd9993b3ac5e62d2275
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-06-19 16:59:23 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View, FlatList,Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import NavegadorStack from './components/navigator/Stack';
-import { elementos } from './requestBackend/dataEjemplo';
-
-
-
-
+import {Provider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <NavegadorStack/>
-    </NavigationContainer>
-    // <View style={{backgroundColor:'#feb529', paddingTop:80, marginTop:90}}>
-    //   <MenuFlotanteOrganizador/>
-    // </View>
+    <Provider>
+      <NavigationContainer>
+        <NavegadorStack/>
+      </NavigationContainer>
+    </Provider>
   );
 }
