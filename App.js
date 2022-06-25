27ab9dcaed85ca6c1861872cd9cc6f77745dbdb8
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-06-25 14:44:36 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import NavegadorStack from './components/navigator/Stack';
 import { elementos } from './requestBackend/dataEjemplo';
 import {Provider } from 'react-native-paper';
+import Perfil from './screens/Perfil';
 
 
 
@@ -14,6 +15,7 @@ export default function App() {
     <Provider>
       <NavigationContainer>
         <NavegadorStack/>
+        {/* <Perfil/> */}
       </NavigationContainer>
     </Provider>
     // <View style={{backgroundColor:'#feb529', paddingTop:80, marginTop:90}}>
