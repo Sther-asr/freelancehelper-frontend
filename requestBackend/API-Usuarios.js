5c7691be802b6516a5bde1cb35eda02fb0212e1e
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-06-28 22:55:19 -0400

    Add files via upload

--- requestBackend/API-Usuarios.js
+++ requestBackend/API-Usuarios.js
@@ -1,4 +1,5 @@
 import APISERVER from "./ConfigAPI";
+
 //funion de consulta usuarios nuevos
 export const inicioSesion = async (infoSesion) =>{
 
@@ -25,5 +26,21 @@ export const cambioContrasena = async (infoSolicitud) =>{
             body: JSON.stringify(infoSolicitud)
         }
     );
+    return await respuesta.json();
+}
+
+//funcion para consultar los datos del usuario
+export const consultaDatosUsuario = async (infoSesion) =>{
+    const respuesta = await fetch(
+        `${APISERVER}/usuarios/consulta/id`,
+        {
+            method:"POST",
+            headers: {Accept:"application/json", "Content-Type":"application/json"},
+            body: JSON.stringify(infoSesion)
+        }
+    )
+    .catch(function(error) {
+        console.log('Error durante la promesa para consultar los datos del usuario' + error.message);
+        });
     return await respuesta.json();
 }
