e5a4c6660be68de6e29409cb1ad58084661ca6c3
Sther-asr <86372495+Sther-asr@users.noreply.github.com>
2022-06-28 22:59:52 -0400

    Update API-Diarias.js

--- requestBackend/API-Diarias.js
+++ requestBackend/API-Diarias.js
@@ -52,3 +52,21 @@ export const consultaMovimientos= async (infoSolicitud) =>{
     //console.log('Este es el resultado' +  JSON.stringify(respuesta));
     return await respuesta.json();
 }
+
+//funion de consultas de movimientos
+export const consultaMontoTotalMovimientos= async (infoSolicitud) =>{
+    //console.log('Datos' + JSON.stringify(infoSolicitud));
+    const respuesta = await fetch(
+        `${APISERVER}/consultas/consultaMontoTotalMovimientos`,
+        {
+            method:"POST",
+            headers: {Accept:"application/json", "Content-Type":"application/json"},
+            body: JSON.stringify(infoSolicitud)
+        }
+    )
+    .catch(function(error) {
+        console.log('Error durante la promesa de consultas diarias' + error.message);
+        });
+    //console.log('Este es el resultado' +  JSON.stringify(respuesta));
+    return await respuesta.json();
+}
